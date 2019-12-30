8f1df10bd27ce56cd24c7e027b5a6cba8c808161
glitchgirl <londonspleen@gmail.com>
2019-12-30 11:33:08 -0500

    progress on day2

--- process.js
+++ process.js
@@ -1,4 +1,11 @@
 var orderText = document.getElementById("order");
+
+var menu = {
+    "fries":19,
+    "shakes":23,
+    "burgers":45
+};
+
 orderText.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
         repeatIt();
@@ -12,4 +19,13 @@ function repeatIt(){
     var textnode = document.createTextNode(customerOrder);
     node.appendChild(textnode);
     document.getElementById("listOrder").appendChild(node);
+    parseOrder(customerOrder);
+}
+
+function parseOrder(custOrder){
+    custOrderArr = custOrder.split(",");
+    let trimedArr = custOrderArr.map(x => x.trim());
+    let numberArr = trimedArr.map(y => y.split(":"));
+    let processArr = numberArr.map(z => z.trim());
+    console.log(processArr);
 }
