b413a9f78233b1e562a78cb0a50cc14acacbe89d
glitchgirl <londonspleen@gmail.com>
2020-01-06 11:56:54 -0500

    day 2

--- process.js
+++ process.js
@@ -1,31 +1,46 @@
-var orderText = document.getElementById("order");
+/* eslint-disable prefer-template */
+/* eslint-disable no-console */
+const orderText = document.getElementById('order');
 
-var menu = {
-    "fries":19,
-    "shakes":23,
-    "burgers":45
+const menu = {
+  fries: 19,
+  shakes: 23,
+  burgers: 45,
 };
 
-orderText.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-        repeatIt();
+function parseOrder(custOrder) {
+  return custOrder
+    .split(',')
+    .map((x) => x.trim())
+    .flatMap((y) => y.split(':').map((x) => x.trim()));
+}
+function print(){
+  // move this from check order and repeat it, reduce rep
+}
+function checkOrder(prettyOrder) {
+  for(i =1; i < prettyOrder.length; i +=2){
+    if(!(prettyOrder[i] in menu)){
+      console.log(prettyOrder[i]);
+      const menuError = "That item isn't on the menu"
+      const node = document.createElement('LI');
+      const textnode = document.createTextNode(menuError);
+      node.appendChild(textnode);
+      document.getElementById('listOrder').appendChild(node);
     }
+  }
+}
+function repeatIt() {
+  const customerOrder = document.getElementById('order').value;
+  const input = document.getElementById('order');
+  input.value = '';
+  const node = document.createElement('LI');
+  const textnode = document.createTextNode(customerOrder);
+  node.appendChild(textnode);
+  document.getElementById('listOrder').appendChild(node);
+  checkOrder(parseOrder(customerOrder));
+}
+orderText.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) { // checks whether the pressed key is "Enter"
+    repeatIt();
+  }
 });
-function repeatIt(){
-    var customerOrder = document.getElementById("order").value;
-    var input = document.getElementById("order");
-    input.value = "";
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(customerOrder);
-    node.appendChild(textnode);
-    document.getElementById("listOrder").appendChild(node);
-    parseOrder(customerOrder);
-}
-
-function parseOrder(custOrder){
-    custOrderArr = custOrder.split(",");
-    let trimedArr = custOrderArr.map(x => x.trim());
-    let numberArr = trimedArr.map(y => y.split(":"));
-    let processArr = numberArr.map(z => z.trim());
-    console.log(processArr);
-}
